9c1f94c20624c1a42ed57f78ccda0769e0e0c35a
Aym <aymeric.boisne-noc@3wa.io>
2024-06-03 10:07:00 +0200

    first commit

---
+++ src/components/PlantItem.js
@@ -0,0 +1,22 @@
+import CareScale from './CareScale'
+import '../styles/PlantItem.css'
+
+function PlantItem({ id, cover, name, water, light }) {
+	return (
+		<li key={id} className='lmj-plant-item' onClick={()=>handleClick(name)}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
+    function handleClick(plantName){
+        console.log('event:', plantName)
+        alert(`Vous avez acheter 1 ${plantName} ? Très bon choix ! 🔥`)
+    }
+}
+
+
+export default PlantItem
